b7a321aadb86637200863afb52c72ee95e974f7c
菊永0909 <masashi.kikunaga@ipeplus.net>
2020-08-18 23:03:18 +0900

    第9章

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
 
     def index
-        
+        @messages = Message.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "messages/index", to: "messages#index"
-  get "/", to: "messages#index"
+  get "/", to: "messages#index", as: "root"
 end
